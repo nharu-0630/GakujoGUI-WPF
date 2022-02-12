30c796709b327f1e28f03a12533d4821ee126dde
xyzyxJP <sahashi2002@gmail.com>
2022-02-13 00:45:42 +0900

    Update AssemblyInfo.cs

--- GakujoGUI/AssemblyInfo.cs
+++ GakujoGUI/AssemblyInfo.cs
@@ -1,4 +1,3 @@
-using System.Reflection;
 using System.Windows;
 
 [assembly: ThemeInfo(
@@ -9,5 +8,3 @@ using System.Windows;
                                               //(used if a resource is not found in the page,
                                               // app, or any theme specific resource dictionaries)
 )]
-
-[assembly: AssemblyVersion("1.0.0.0")]
