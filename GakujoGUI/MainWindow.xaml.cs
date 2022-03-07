0dfa982f7d4243534cf22607b09a72046cfb2eec
Yuto Sahashi <sahashi2002@gmail.com>
2022-03-07 15:00:20 +0900

    Update MainWindow.xaml.cs

--- GakujoGUI/MainWindow.xaml.cs
+++ GakujoGUI/MainWindow.xaml.cs
@@ -58,7 +58,9 @@ namespace GakujoGUI
 
         private bool shutdownFlag = false;
 
+#pragma warning disable CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
         public MainWindow()
+#pragma warning restore CS8618 // null 非許容のフィールドには、コンストラクターの終了時に null 以外の値が入っていなければなりません。Null 許容として宣言することをご検討ください。
         {
             InitializeComponent();
             LoggingConfiguration loggingConfiguration = new();
