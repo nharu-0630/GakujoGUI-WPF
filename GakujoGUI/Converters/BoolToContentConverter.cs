07d8786d9aed8de91a81164f7382f706683e108e
Yuto Sahashi <sahashi2002@gmail.com>
2022-07-18 21:04:55 +0900

    replace subjects

--- GakujoGUI/Converters/BoolToContentConverter.cs
+++ GakujoGUI/Converters/BoolToContentConverter.cs
@@ -2,7 +2,7 @@
 using System.Globalization;
 using System.Windows.Data;
 
-namespace GakujoGUI
+namespace GakujoGUI.Converters
 {
     public class BoolToContentConverter : IValueConverter
     {
---
+++ GakujoGUI/Converters/StringToSubjectsConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows.Data;
+
+namespace GakujoGUI.Converters
+{
+    public class StringToSubjectsConverter : IValueConverter
+    {
+        public StringToSubjectsConverter()
+        {
+            NullContent = "Null";
+        }
+
+        public object NullContent { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) { return NullContent; }
+            string stringValue = "";
+            bool isString = true;
+            try { stringValue = (string)value; }
+            catch { isString = false; }
+            if (!isString) { return NullContent; }
+            return Regex.Replace(stringValue, "（.*）(前|後)期.*", "");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
